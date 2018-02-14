3ec1902a3b4d84923876c7ee07408e0f25c77bb2
Emily Luong <eluong97@gmail.com>
2018-02-13 23:24:00 -0800

    added candies eaten

--- script.js
+++ script.js
@@ -7,19 +7,17 @@ console.log('reading js');
 // declare and populate array
 var hearts = ['images/honey.png', 'images/adore.png', 'images/lucky.png', 'images/ily.png', 'images/sweetheart.png', 'images/love.png', 'images/happy.png', 'images/sweetpotato.png', 'images/greencard.png', 'images/inlove.png', 'images/mish.png', 'images/fav.png']
 
-//variable for candies
+//declare variable for candies
 var candy = document.getElementById('candy');
+
+//declare variable for box
+var box = document.getElementById('box');
 
 // capture img
 var img=document.getElementsByTagName('img')[0];
 
 //initialize button
 var button=document.getElementById('button');
-
-
-//declare variable for box
-var box = document.getElementById('box');
-
 
 //click button to change img of candy hearts
 button.addEventListener('click', function(){
@@ -33,3 +31,18 @@ button.addEventListener('click', function(){
   //change msg to "more!"
   button.innerHTML="More!";
 })
+
+//count how many candies Eaten
+//declare variable for clicksCount
+var clicksCount=0;
+
+//declare variable for p
+var p = document.getElementsByTagName('p')[0];
+
+//click button to update candies eaten
+button.addEventListener('click', updateClicks);
+function updateClicks(){
+ console.log(clicksCount);
+ clicksCount++;
+ p.innerHTML= "Candies Eaten: "+ clicksCount;
+}
